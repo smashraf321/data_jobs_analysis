f90ba81e35d4552dde0e73cf93de8f129939bc8a
Ashraf Shaikh Mohammed <sma.muhammad.321@gmail.com>
2024-05-29 11:11:48 +0530

    project updates

---
+++ project_sql/3_top_demanded_skills.sql
@@ -0,0 +1,54 @@
+/*
+Question: What are the most in-demand skills for data analysts?
+- Join job postings to inner join table similar to query 2
+- Identify the top 5 in-demand skills for a data analyst
+- Focus on all job postings
+- Why? Retrieves the top 5 skills with the highest demand in the job market,
+    providing insights into the most valuable skills for job seekers.
+*/
+
+SELECT
+    skills,
+    COUNT(skills_job_dim.job_id) AS demand_count
+FROM job_postings_fact
+INNER JOIN skills_job_dim
+    ON job_postings_fact.job_id = skills_job_dim.job_id
+INNER JOIN skills_dim
+    ON skills_job_dim.skill_id = skills_dim.skill_id
+WHERE
+    job_title_short = 'Data Analyst' AND
+    job_work_from_home = TRUE
+GROUP BY
+    skills
+ORDER BY
+    demand_count DESC
+LIMIT 5
+
+/*
+SQL and Excel are very important.
+We also need Programming and Visualization Tools like Python, Tableau, and Power BI
+
+JSON results
+[
+  {
+    "skills": "sql",
+    "demand_count": "7291"
+  },
+  {
+    "skills": "excel",
+    "demand_count": "4611"
+  },
+  {
+    "skills": "python",
+    "demand_count": "4330"
+  },
+  {
+    "skills": "tableau",
+    "demand_count": "3745"
+  },
+  {
+    "skills": "power bi",
+    "demand_count": "2609"
+  }
+]
+*/
--- project_sql/query_1.sql
+++
@@ -1,7 +0,0 @@
--- TEST QUERIES
-
-SELECT *
-FROM company_dim;
-
-SELECT *
-FROM job_postings_fact;
